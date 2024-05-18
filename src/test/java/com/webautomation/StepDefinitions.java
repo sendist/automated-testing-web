edca8aea725de48378232076041a8875ad93f692
sendist <sendisetiawan10902@gmail.com>
2024-05-18 18:11:38 +0700

    initial commit

---
+++ src/test/java/com/webautomation/StepDefinitions.java
@@ -0,0 +1,53 @@
+package com.webautomation;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    private final WebDriver driver = new ChromeDriver();
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+    @Given("I am on the Google search page")
+    public void I_visit_google() {
+        driver.get("https://www.google.com");
+    }
+
+    @When("I search for {string}")
+    public void search_for(String query) {
+        WebElement element = driver.findElement(By.name("q"));
+        // Enter something to search for
+        element.sendKeys(query);
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+   }
+
+   @Then("the page title should start with {string}")
+   public void checkTitle(String titleStartsWith) {
+       // Google's search is rendered dynamically with JavaScript
+       // Wait for the page to load timeout after ten seconds
+   }
+
+   @After()
+   public void closeBrowser() {
+       driver.quit();
+   }
+
+}
---
+++ target/test-classes/com/webautomation/example.feature
@@ -0,0 +1,11 @@
+Feature: An example
+
+  Scenario: The example
+    Given an example scenario
+    When all step definitions are implemented
+    Then the scenario passes
+
+  Scenario: Finding some cheese
+   Given I am on the Google search page
+   When I search for "Cheese!"
+   Then the page title should start with "cheese"
